096f472d74910a06c4a94ae1b5f873d6b01d8602
JohnUFG <joao.tavares@discente.ufg.br>
2023-01-27 23:33:40 -0300

    Adicionando a pasta src

---
+++ mytodo/src/main/java/br/ufg/sep/entity/Prova.java
@@ -0,0 +1,114 @@
+package br.ufg.sep.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="provas")
+public class Prova extends AbstractEntity{
+	
+	@Column
+	private String areaConhecimento;
+	
+	@OneToMany(mappedBy="prova", fetch = FetchType.LAZY,cascade= CascadeType.ALL)
+	private List<Questao> questoes;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="responsavel_id", nullable = false)
+	private Cadastro responsavel;
+
+	@Column
+	private String nivel;
+	
+	@Column
+	private String descricao;
+	
+	
+
+
+	public String getAreaConhecimento() {
+		return areaConhecimento;
+	}
+
+
+
+
+	public void setAreaConhecimento(String areaConhecimento) {
+		this.areaConhecimento = areaConhecimento;
+	}
+
+
+
+
+	public List<Questao> getQuestoes() {
+		return questoes;
+	}
+
+
+
+
+	public void setQuestoes(List<Questao> questoes) {
+		this.questoes = questoes;
+	}
+
+
+
+
+	public Cadastro getResponsavel() {
+		return responsavel;
+	}
+
+
+
+
+	public void setResponsavel(Cadastro responsavel) {
+		this.responsavel = responsavel;
+	}
+
+
+
+
+	public String getNivel() {
+		return nivel;
+	}
+
+
+
+
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+
+
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+
+	public Prova(){
+		
+	}
+	
+
+	
+}
