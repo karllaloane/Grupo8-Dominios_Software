9d53b406a9396f674e231f0f5bf847bfd10cd877
gabrielreisdvs <gabrielreis1994@gmail.com>
2023-02-01 13:48:35 -0300

    view e presenter para edicao de concurso

--- mytodo/src/main/java/br/ufg/sep/views/concurso/EditarConcursoView.java
+++ mytodo/src/main/java/br/ufg/sep/views/concurso/EditarConcursoView.java
@@ -28,7 +28,113 @@ import br.ufg.sep.views.concurso.presenter.EditarConcursoPresenter;
 @Route(value="editar_concurso", layout = MainLayout.class)
 @PageTitle("Concursos")
 @RolesAllowed({"ADMIN", "PED"})
-public class EditarConcursoView extends VerticalLayout {
-
+public class EditarConcursoView extends VerticalLayout implements HasUrlParameter<Long> {
 	
+	Button save;
+	Button cancel;
+	TextField nome;
+	TextField cidade;
+	DatePicker dataInicio;
+	DatePicker dataFim;
+	VerticalLayout layout;
+	HorizontalLayout buttonLayout;
+	Concurso concurso;
+	ConcursoService service;
+	//FormularioConcursoPresenter formPresenter;
+	
+	public EditarConcursoView(ConcursoService cS) {
+		
+		this.service = cS;
+		
+		criarTela();
+		
+		EditarConcursoPresenter formPresenter = new EditarConcursoPresenter(this, cS);
+        
+        setPadding(true);
+        add(layout, buttonLayout);
+        
+	}	
+	
+	private void criarTela() {
+
+		DatePicker.DatePickerI18n singleFormatI18n = new DatePicker.DatePickerI18n();
+		singleFormatI18n.setDateFormat("dd/MM/yyyy");
+		
+		nome = new TextField("Nome", "", "");
+        cidade = new TextField("Cidade", "", "");
+        dataInicio = new DatePicker("Data de Início");
+        dataFim = new DatePicker("Data Fim");
+        
+        dataInicio.setI18n(singleFormatI18n);
+        dataFim.setI18n(singleFormatI18n);
+        dataFim.setPlaceholder("DD/MM/AAAA");
+        dataInicio.setPlaceholder("DD/MM/AAAA");
+        
+        FormLayout formLayout = new FormLayout(nome, cidade);
+        FormLayout formData = new FormLayout(dataInicio, dataFim);
+        formLayout.setResponsiveSteps(new ResponsiveStep("0", 1));
+        formData.setResponsiveSteps(new ResponsiveStep("0", 2));
+        //formLayout.setMaxWidth("700px");
+        //formData.setMaxWidth("700px");
+
+        save = new Button("Salvar alterações");
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        cancel = new Button("Cancelar");
+        
+        layout = new VerticalLayout(formLayout, formData);
+        //layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.setMaxWidth("700px");
+
+        buttonLayout = new HorizontalLayout(save, cancel);
+        buttonLayout.setPadding(true);
+	}
+
+	public Button getSave() {
+		return save;
+	}
+
+	public TextField getNome() {
+		return nome;
+	}
+
+	public TextField getCidade() {
+		return cidade;
+	}
+
+	public DatePicker getDataInicio() {
+		return dataInicio;
+	}
+
+	public DatePicker getDataFim() {
+		return dataFim;
+	}
+	
+	public Button getCancel() {
+		return cancel;
+	}
+
+	@Override
+	public void setParameter(BeforeEvent event, Long parameter) {
+		// TODO Auto-generated method stub
+		Optional<Concurso> optionalConcurso = service.getRepository().findById(parameter);
+		if (optionalConcurso.isPresent()) {
+			concurso = optionalConcurso.get();
+			
+			this.nome.setValue(concurso.getNome());
+			this.cidade.setValue(concurso.getCidade());
+			this.dataFim.setValue(concurso.getDataFim());
+			this.dataInicio.setValue(concurso.getDataInicio());
+			
+		} else {
+			Notification notification = Notification
+			        .show("Impossível acessar o concurso");
+			notification.setPosition(Position.TOP_CENTER);
+			notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+		}
+		
+	}
+	
+	public Concurso getConcurso() {
+		return concurso;
+	}
 }
--- mytodo/src/main/java/br/ufg/sep/views/concurso/presenter/EditarConcursoPresenter.java
+++ mytodo/src/main/java/br/ufg/sep/views/concurso/presenter/EditarConcursoPresenter.java
@@ -18,6 +18,72 @@ import br.ufg.sep.views.concurso.FormularioConcursoView;
 
 public class EditarConcursoPresenter  {
 
-
+	Concurso concurso;
+	ConcursoService concursoService;
+	
+	public EditarConcursoPresenter(EditarConcursoView editarConcursoView, ConcursoService cS) {
+		//concursoView = formularioConcursoViewImpl;
+		this.concursoService = cS;
+		
+		
+		editarConcursoView.getCancel().addClickListener(e->{
+			 editarConcursoView.getCancel().getUI().ifPresent(ui->{
+			 ui.navigate(ConcursosView.class); });
+		});
+		
+		editarConcursoView.getSave().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+			
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				// TODO Auto-generated method stub
+				salvarConcurso(event, editarConcursoView);				
+			}
+		});
+	}
+	
+	private void salvarConcurso(ClickEvent<Button> event, EditarConcursoView concursoView) {
+		String nomeConcurso = concursoView.getNome().getValue();	
+		String nomeCidade = concursoView.getCidade().getValue();
+		LocalDate dataInicio;
+		LocalDate dataFim;
+		
+		//verificando campos em branco
+		if(nomeCidade.isEmpty() || nomeConcurso.isEmpty() || concursoView.getDataInicio().isEmpty()
+				|| concursoView.getDataFim().isEmpty()) {
+			
+			Notification notification = Notification
+			        .show("Campos em branco!");
+			notification.setPosition(Position.TOP_CENTER);
+			notification.addThemeVariants(NotificationVariant.LUMO_CONTRAST);
+			return;
+		}
+		
+		dataInicio = concursoView.getDataInicio().getValue();
+		dataFim = concursoView.getDataFim().getValue();
+			
+		//validando a data
+		if(dataInicio.isAfter(dataFim)) {
+			Notification notification = Notification
+			        .show("A data de início não pode ser posterior a data final!");
+			notification.setPosition(Position.TOP_CENTER);
+			notification.addThemeVariants(NotificationVariant.LUMO_CONTRAST);
+			return;
+		}
+			
+		Concurso concurso = new Concurso();
+		concurso.setNome(nomeConcurso);
+		concurso.setCidade(nomeCidade);
+		concurso.setDataInicio(dataInicio);
+		concurso.setDataFim(dataFim);
+		concurso.setId(concursoView.getConcurso().getId());
+			
+		concursoService.save(concurso);		
+		
+		Notification notification = Notification
+		        .show("Alterações salvas com sucesso!");
+		notification.setPosition(Position.TOP_CENTER);
+		notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+		event.getSource().getUI().ifPresent(ui -> ui.navigate(ConcursosView.class));						
+	}
 
 }
