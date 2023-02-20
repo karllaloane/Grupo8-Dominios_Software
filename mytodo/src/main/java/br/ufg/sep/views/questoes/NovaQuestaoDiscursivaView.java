28a0be8b0b748e74b445069d24f65c8d0efca2cd
gabrielreisdvs <gabrielreis1994@gmail.com>
2023-02-20 07:47:54 -0300

    adicionando implementacao do view e presenter da NovaQuestaoDiscursiva

--- mytodo/src/main/java/br/ufg/sep/views/questoes/NovaQuestaoDiscursivaView.java
+++ mytodo/src/main/java/br/ufg/sep/views/questoes/NovaQuestaoDiscursivaView.java
@@ -1,5 +1,171 @@
 package br.ufg.sep.views.questoes;
 
-public class NovaQuestaoDiscursivaView {
+import java.util.Optional;
 
+import javax.annotation.security.PermitAll;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import br.ufg.sep.data.services.ProvaService;
+import br.ufg.sep.data.services.QuestaoService;
+import br.ufg.sep.entity.Prova;
+import br.ufg.sep.views.MainLayout;
+import br.ufg.sep.views.questoes.presenter.NovaQuestaoDiscursivaPresenter;
+
+@Route(value="cadastrar_questoes_discursiva", layout = MainLayout.class)
+@PageTitle("Cadastrar Questão Discursiva")
+@PermitAll
+public class NovaQuestaoDiscursivaView extends VerticalLayout implements HasUrlParameter<Long>{
+	
+	private ProvaService provaService;
+	private QuestaoService questaoService;
+	private NovaQuestaoDiscursivaPresenter presenter;
+	
+	private MetadadosQuestaoComponent metadados;
+	
+	//layouts final
+	//todos eles são criados no construtor
+	//e adicionados posteriormente
+	private VerticalLayout layoutFinal; 
+	private VerticalLayout buttonsLayout;
+	
+	//inputs da questao
+	private TextArea enunciado;	
+	private TextArea respostaEsperada;	
+	
+	private Button salvarButton;
+	private Button enviarButton;
+	
+	private Prova prova;
+	
+	public NovaQuestaoDiscursivaView(ProvaService provaService, QuestaoService questaoService) {
+		this.provaService = provaService;
+		this.questaoService = questaoService;
+		
+		metadados = new MetadadosQuestaoComponent();
+		
+		//criando os layouts intermediarios
+		HorizontalLayout informacaoLayout = new HorizontalLayout();
+		VerticalLayout enunciadoLayout = new VerticalLayout();
+		VerticalLayout respostaEsperadaLayout = new VerticalLayout();
+		
+		//criando os layouts principais
+		layoutFinal = new VerticalLayout();
+		buttonsLayout = new VerticalLayout();
+		
+		//ajustando tamanho dos layouts
+		enunciadoLayout.setWidth("667px");
+		informacaoLayout.setWidth("700px");
+		respostaEsperadaLayout.setWidth("667px");
+		buttonsLayout.setWidth("685px");
+		
+		informacaoLayout.add(metadados);
+		
+		/**************** Layout do enunciado ***********************/
+		
+		//criando os componentes do layout de questao
+		Label enunciadoLabel = new Label("Enunciado");
+		enunciado = new TextArea();
+		
+		//alterando estilos
+		enunciado.setWidthFull();
+		enunciado.setMinHeight("150px");
+
+		enunciadoLayout.add(enunciadoLabel, enunciado);
+		
+		//estilo do layout do enunciado
+		enunciadoLayout.setSpacing(false);
+		enunciadoLayout.setPadding(false);
+		
+		/**************** Layout da resposta esperada ***********************/
+		
+		//criando os componentes
+		Label labelRespEsperada = new Label("Resposta Esperada");
+		respostaEsperada = new TextArea();
+		
+		//alterando estilos
+		respostaEsperada.setWidthFull();
+		respostaEsperada.setMinHeight("150px");
+		
+		respostaEsperadaLayout.add(labelRespEsperada, respostaEsperada);
+		
+		//estilo do layout do enunciado
+		respostaEsperadaLayout.setSpacing(false);
+		respostaEsperadaLayout.setPadding(false);
+		
+		addBotoes();
+		
+		layoutFinal.add(informacaoLayout, enunciadoLayout, respostaEsperadaLayout, buttonsLayout);
+		
+		add(layoutFinal);
+	}
+	
+	@Override
+	public void setParameter(BeforeEvent event, Long parameter) {
+		// TODO Auto-generated method stub
+		
+		Optional<Prova> optionalProva = provaService.getRepository().findById(parameter);
+		if (optionalProva.isPresent()) {
+			setProva(optionalProva.get());
+			//this.provaId = prova.getId();
+			
+			
+
+			this.presenter = new NovaQuestaoDiscursivaPresenter(provaService, questaoService, this); //iniciar o presenter
+		}
+		
+	}
+
+	private void addBotoes() {
+		//critando botoes
+		
+		HorizontalLayout h = new HorizontalLayout();
+		
+		salvarButton = new Button("Salvar");
+		
+		//este botão poderá sofrer mudanças de acordo com o status da questão
+		enviarButton = new Button("Enviar para Revisão Técnica 1");
+		
+		h.add(salvarButton, enviarButton);
+		//h.setAlignItems(Alignment.END);
+		
+		buttonsLayout.add(h);
+		buttonsLayout.setAlignItems(Alignment.END);
+	}
+
+	public Prova getProva() {
+		return prova;
+	}
+
+	public void setProva(Prova prova) {
+		this.prova = prova;
+	}
+	
+	public Button getSalvarButton() {
+		return salvarButton;
+	}
+
+	public Button getEnviarButton() {
+		return enviarButton;
+	}
+	
+	public MetadadosQuestaoComponent getMetadados() {
+		return metadados;
+	}
+
+	public TextArea getEnunciado() {
+		return enunciado;
+	}
+	
+	public TextArea getRespostaEsperada() {
+		return respostaEsperada;
+	}
 }
--- mytodo/src/main/java/br/ufg/sep/views/questoes/presenter/NovaQuestaoDiscursivaPresenter.java
+++ mytodo/src/main/java/br/ufg/sep/views/questoes/presenter/NovaQuestaoDiscursivaPresenter.java
@@ -1,6 +1,134 @@
 package br.ufg.sep.views.questoes.presenter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.notification.Notification.Position;
+
+import br.ufg.sep.data.services.ProvaService;
+import br.ufg.sep.data.services.QuestaoService;
+import br.ufg.sep.entity.NivelDificuldade;
+import br.ufg.sep.entity.Prova;
+import br.ufg.sep.entity.Questao;
+import br.ufg.sep.entity.QuestaoDiscursiva;
+import br.ufg.sep.entity.QuestaoObjetiva;
+import br.ufg.sep.views.questoes.NovaQuestaoDiscursivaView;
+import br.ufg.sep.views.questoes.NovaQuestaoObjetivaView;
+import br.ufg.sep.views.questoes.QuestoesProvaView;
 
 public class NovaQuestaoDiscursivaPresenter {
+	
+	private ProvaService provaService;
+	private QuestaoService questaoService;
+	private NovaQuestaoDiscursivaView view;
+	private Questao questao;
+	private Prova prova;
+	
+	public NovaQuestaoDiscursivaPresenter(ProvaService provaService,
+			QuestaoService questaoService, NovaQuestaoDiscursivaView view) {
+		
+		this.provaService = provaService;
+		this.questaoService = questaoService;
+		this.view = view;
+		
+		//chama o metodo salvar, que salva mas nao envia a questao para relaboracao
+		view.getSalvarButton().addClickListener( e->salvarQuestao(e));
+		
+		//chama o metodo enviar, que salva e envia a questao para relaboracao
+		view.getEnviarButton().addClickListener( e->enviarQuestao(e));	
+	}
+	
+	private void enviarQuestao(ClickEvent<Button> event) {
+		Notification notification;
+		questao = new QuestaoDiscursiva();
+		prova = view.getProva();
+		
+		//chama o método para coletar os dados da view
+		//verificando se ele retornou ture, ou seja
+		//todos os dados foram coletados com sucesso
+		if(coletarDados()) {
+
+			//envia para a correcao
+			questao.enviarParaRevisao(null);
+			
+			//salva a questao
+			questaoService.getRepository().save(questao);;
+			
+			/*Notifica ação bem sucedida*/
+			notification = Notification
+			        .show("Questão enviada para revisao 1 com sucesso!");
+			notification.setPosition(Position.TOP_CENTER);
+			notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+			
+			event.getSource().getUI().ifPresent(ui -> ui.navigate(QuestoesProvaView.class, prova.getId()));
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void salvarQuestao(ClickEvent<Button> event) {
+		
+		Notification notification;
+		questao = new QuestaoDiscursiva();
+		prova = view.getProva();
+		
+		//chama o método para coletar os dados da view
+		//verificando se ele retornou ture, ou seja
+		//todos os dados foram coletados com sucesso
+		if(coletarDados()) {
+			prova.getQuestoes().add(questao);
+			
+			provaService.save(prova);
+			
+			/*Notifica ação bem sucedida*/
+			notification = Notification
+			        .show("Questão salva com sucesso!");
+			notification.setPosition(Position.TOP_CENTER);
+			notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+			
+			event.getSource().getUI().ifPresent(ui -> ui.navigate(QuestoesProvaView.class, prova.getId()));
+		}
+		
+	}
+	
+	private boolean coletarDados() {
+		Notification notification;
+		
+		/* Criando e armazenando os valores do Input*/
+		NivelDificuldade nivelSelecionado = view.getMetadados().getNivelDificuldadeCombo().getValue();
+		String enunciado = view.getEnunciado().getValue();
+		String respostaEsperada = view.getRespostaEsperada().getValue();
+		List<String> subarea = view.getMetadados().getSubAreas();
+							
+		//pra nao deixar salvar sem selecionar o nivel de dificuldade
+		if(nivelSelecionado == null) {
+			notification = Notification
+			        .show("Selecione o nível de dificuladade." );
+			notification.setPosition(Position.TOP_CENTER);
+			notification.addThemeVariants(NotificationVariant.LUMO_CONTRAST);
+			return false;
+		}
+
+		//verificando demais campos em branco
+		if(enunciado.isEmpty() || respostaEsperada.isEmpty() || subarea.size() == 0) {
+			notification = Notification
+			        .show("Campos em branco." );
+			notification.setPosition(Position.TOP_CENTER);
+			notification.addThemeVariants(NotificationVariant.LUMO_CONTRAST);
+			return false;
+		}
+		
+		//setando os dados do objeto
+		questao.setSubAreas(subarea);
+		questao.setEnunciado(enunciado);
+		((QuestaoDiscursiva) questao).setRespostaEsperada(respostaEsperada);
+		questao.setNivelDificuldade(nivelSelecionado);
+		questao.setProva(view.getProva());
+		
+		return true;
+	}
 
 }
